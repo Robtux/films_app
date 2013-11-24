dbfc5e5588762bbd4906e2b0aac2077255990565
Robtux <zaniol.roberto@gmail.com>
2013-11-24 20:18:39 +0100

    Signin, Signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 FilmsApp::Application.routes.draw do
-  get "users/new"
-  root 'static_pages#home'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  root  'static_pages#home'
+  match '/registrati',  to: 'users#new',            via: 'get'
+  match '/accedi',  to: 'sessions#new',         via: 'get'
+  match '/esci', to: 'sessions#destroy',     via: 'delete'
   match '/help',    	to: 'static_pages#help',	via: 'get'
   match '/about',   	to: 'static_pages#about',	via: 'get'
   match '/contact', 	to: 'static_pages#contact',	via: 'get'  
-  match '/registrati', 	to: 'users#new', 		via: 'get'
   resources :directors
 
   resources :genres
